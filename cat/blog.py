72831a71e95a5f6e848f2174475016705d3c6e31
Tyler Williams <tyler@ogdenstudios.xyz>
2018-05-18 16:30:17 -0600

    Added a scrollable feed to the landing page

--- cat/blog.py
+++ cat/blog.py
@@ -11,12 +11,12 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
-    activities = db.execute(
-        'SELECT p.id, title, created, author_id, username, points, note'
-        ' FROM action p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+    actions = db.execute(
+    'SELECT title, author_id, points, note'
+    ' FROM action'
+    ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('blog/index.html', activities=activities)
+    return render_template('blog/index.html', actions=actions)
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
