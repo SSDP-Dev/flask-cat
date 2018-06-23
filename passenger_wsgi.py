48fe7cfab5b8c8dadc2abd0582b8215b537d8c4c
Tyler <tyler@ogdenstudios.xyz>
2018-06-22 20:23:08 -0600

    Don't need passenger config

--- passenger_wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-#
-INTERP = "/home/username/directory/cat/venv/bin/python3"
-#INTERP is present twice so that the new Python interpreter knows the actual executable path
-if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-sys.path.append(os.getcwd())
-
-sys.path.append('cat')
-from cat import app as application
